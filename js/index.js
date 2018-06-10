f8b8056edf4420097c94f851fe91d7ed91cc75b6
Efeex <matteo.foroni97@gmail.com>
2018-06-10 13:11:10 +0200

    removed punctation and reduced Query

--- js/index.js
+++ js/index.js
@@ -12,27 +12,31 @@ $.getJSON({
     let item = prodotti[i]
     let suggestion = item.serving_suggestion,
       categoria = item.primary_category
+      console.log(suggestion)
 
     if (suggestion == null) {
       console.error("suggestion not found")
     } else {
-      let suggestionlowercase = suggestion.toLowerCase() 
-      let splitQuery = suggestionlowercase.split(" "),
+      let suggestionLowercase = suggestion.toLowerCase() // tolgo le maiuscole dalla stringa
+          suggetsionNoDot = suggestionLowercase.replace(/[^\w\s]|_/g, "") //rimuovo la punteggiatura
+      let splitQuery = suggetsionNoDot.split(" "), // divido la frase in singole parole
           sw = stopwords.en
-          filteredString = splitQuery.filter(function(parola){
-            if (sw.includes(parola)){ return false
+          filteredString = splitQuery.filter(function(parola){ //filtro mantenendo le parole non presenti nel file stopwords
+            if (sw.includes(parola)){ 
+              return false
              } else {
                return true
              }
-          })    
-
-      console.log(filteredString.join)
-
+          })
+          let lastWords = filteredString.slice(-4, filteredString.length)
+        
+          console.log(lastWords)
+          
       //seconda chiamata per le ricette
 
       let apiKey = "aa14fbbd2e93d8dee453a8bc3f5fee12"
       apiID = "69e8573d"
-      url2 = "https://api.edamam.com/search?q=" + filteredString.join(" ") + "&app_id=" + apiID + "&app_key=" + apiKey
+      url2 = "https://api.edamam.com/search?q=" + lastWords.join(" ") + "&app_id=" + apiID + "&app_key=" + apiKey
       console.log(url2)
 
       $.getJSON({
