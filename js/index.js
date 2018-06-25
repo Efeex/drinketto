1747af46c6658fd7d5540dde5369c3e633180371
Efeex <matteo.foroni97@gmail.com>
2018-06-25 11:07:24 +0200

    fixed css

--- js/index.js
+++ js/index.js
@@ -1,36 +1,40 @@
 //seleziono ed inizializzio il container per il template di Handlebars
 $(function () {
   var beerCardTpl = Handlebars.compile($("#beer-card").html()),
-      recipeCardTpl = Handlebars.compile($("#recipe-card").html()),
-      homeTpl = Handlebars.compile($("#homepage").html()),
-      container = $("#drink-container")
+    recipeCardTpl = Handlebars.compile($("#recipe-card").html()),
+    homeTpl = Handlebars.compile($("#homepage").html()),
+    container = $("#drink-container")
 
-      
-      $(homeTpl()).appendTo($(".homepage-container"));
 
-  $('.navbar-brand').click(function(e){    
+  $(homeTpl()).appendTo($(".homepage-container"));
+
+  $('.navbar-brand').click(function (e) {
     $(".homepage-container").empty()
     $("#drink-container").empty()
     $(homeTpl()).appendTo($(".homepage-container")); // torno alla homepage
   })
-  
+
 
   $('#navbarSupportedContent .nav-link').click(function (e) {
     $(".homepage-container").empty()
     $("#drink-container").empty()
-    getData($(this).text()) //prendo il valore dal testo dei link nella navbar per la ricerca
+    let position = getData($(this).text()) //prendo il valore dal testo dei link nella navbar per la ricerca
+    // if (position = "Wine") {
+    //  $("#nav").addClass("bg-color-v")
+    //  $(".card-header").addClass("bg-color-v")
+    //}
   })
 
-  $('.formQuery').submit (function submitQuery(event) { //funzione per la ricerca libera
+  $('.formQuery').submit(function submitQuery(event) { //funzione per la ricerca libera
     let userQuery = event.target[0].value //ricavo il valore dalle parole immesse dall'utente
     $(".homepage-container").empty()
     $("#drink-container").empty()
     getData(userQuery)
     event.preventDefault()
-})
+  })
 
-// funzione per ricavare i dati
-  function getData(q) { 
+  // funzione per ricavare i dati
+  function getData(q) {
     $.getJSON({ // prima chiamata per le bevande
       url: 'https://lcboapi.com/products?q=' + q,
       headers: {
@@ -43,7 +47,7 @@ $(function () {
       let prodotti = data.result
 
       prodotti.forEach(function (beer) { //per ogni birra trovata 
-      
+
         let beerCard = $(beerCardTpl(beer)).appendTo(container); //crea una card "appesa" al conteiner (Handlebars)        
         $(".popoverSuggestion").hover().popover('enable');
         beerCard.find(".btn").click(function (event) { //seleziono il bottone al click
@@ -70,36 +74,37 @@ $(function () {
             let apiKey = "aa14fbbd2e93d8dee453a8bc3f5fee12"
             apiID = "69e8573d"
             url2 = "https://api.edamam.com/search?q=" + lastWords.join(" ") + "&app_id=" + apiID + "&app_key=" + apiKey // creo l'url riunendo le ultime parole con il metodo join
-            
+
             $.getJSON({
               url: url2
             }).then(function (recipes) {
 
-                let ricette = recipes.hits[0].recipe,
-                    container = $('#modal-container'),
-                    recipeCard = container.html(recipeCardTpl(ricette)),
-                    suggestiOnRecipe = $('.recipeSuggestion').append("This is our suggestion for your drink: " + suggestion)
-                    ingredienti = ricette.ingredients
-                   
-                $('#recipe-modal').modal().on('hidden.bs.modal', function (e) {
-                  $(this).modal('dispose')
-                }) 
-                $('.btn-danger').click(function () {
-                  $("#recipe-modal").modal('hide')
-                }) 
+              let ricette = recipes.hits[0].recipe,
+                container = $('#modal-container'),
+                recipeCard = container.html(recipeCardTpl(ricette)),
+                suggestiOnRecipe = $('.recipeSuggestion').append("This is our suggestion for your drink: " + suggestion)
+              ingredienti = ricette.ingredients
+
+              $('#recipe-modal').modal().on('hidden.bs.modal', function (e) {
+                $(this).modal('dispose')
+              })
+              $('.btn-danger').click(function (e) {
+                $("#recipe-modal").modal('hide')
+                e.preventDefault()
+              })
             })
           }
         })
       })
-        $(".card").mouseover(function(){
-          $(this).addClass("hvr-glow")
-         })
-         $(".card").mouseout(function(){
-           $(this).removeClass("hvr-glow")
-          })
-          $(".popoverSuggestion").click(function(){
-            $(this).addClass("hvr-icon-pop")
-           })
+      $(".card").mouseover(function () {
+        $(this).addClass("hvr-glow")
+      })
+      $(".card").mouseout(function () {
+        $(this).removeClass("hvr-glow")
+      })
+      $(".popoverSuggestion").click(function () {
+        $(this).addClass("hvr-icon-pop")
+      })
     })
   }
 });
